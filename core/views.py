6e4135306b2addc7fd41d8566f55ecd7e434ce61
chaosishere <148432485+chaosishere@users.noreply.github.com>
2024-04-18 07:29:31 +0530

    dj_leaflet

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from .models import Vehicle
 
 # Create your views here.
 def index(request):
-    context = {}
+    context = {'Vehicle': list(Vehicle.objects.values('latitude', 'longitude'))}
     return render(request, 'index.html', context)
